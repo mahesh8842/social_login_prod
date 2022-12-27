752c6b931aca99636d80d9510a802230f4d13df3
mahesh8842 <55652610+mahesh8842@users.noreply.github.com>
2022-12-27 16:24:40 +0530

    updated static path

--- Social_auth/settings.py
+++ Social_auth/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'Social_auth/static'
+    BASE_DIR / 'Social_auth/staticfiles_build/static'
 ]
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
